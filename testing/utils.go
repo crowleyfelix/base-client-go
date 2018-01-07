f8de42158b178d231b07c36c6d40470e952867e0
Gabriel Felix <crowleyfelix@gmail.com>
2018-01-07 21:57:59 -0200

    Retira base suite

--- testing/utils.go
+++ testing/utils.go
@@ -8,23 +8,19 @@ import (
 	"github.com/stretchr/testify/suite"
 )
 
-type Suite struct {
-	suite.Suite
-}
-
-func (suite *Suite) File(path string) []byte {
+func File(path string) []byte {
 	blob, err := ioutil.ReadFile(path)
 	Expect(err).Should(BeNil())
 	return blob
 }
 
-func (suite *Suite) LoadJSON(path string, target interface{}) {
+func LoadJSON(path string, target interface{}) {
 	blob := suite.File(path)
 	err := json.Unmarshal(blob, &target)
 	Expect(err).Should(BeNil())
 }
 
-func (suite *Suite) Config() *config {
+func Config() *config {
 	c := new(config)
 	c.Load()
 	c.Validate()
