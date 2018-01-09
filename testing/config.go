38932ac648e84e856daf456ec0131a196cb245de
Gabriel Felix <crowleyfelix@gmail.com>
2018-01-08 23:15:09 -0200

    Altera configurações

--- testing/config.go
+++ testing/config.go
@@ -9,10 +9,7 @@ import (
 )
 
 type config struct {
-	URL       string
-	AppKey    string
-	SecretKey string
-	User      string
+	URL string
 }
 
 const (
@@ -27,9 +24,6 @@ func (c *config) Load() {
 
 	fs := flag.NewFlagSet(os.Args[0], 0)
 	fs.StringVar(&c.URL, "url", "", "Url do Cadu")
-	fs.StringVar(&c.AppKey, "app_key", "", "Application Key do Cadu")
-	fs.StringVar(&c.SecretKey, "secret_key", "", "Secret Key do Cadu")
-	fs.StringVar(&c.SecretKey, "user", "", "Secret Key do Cadu")
 
 	err := fs.Parse(os.Args[1:])
 	if err != nil {
@@ -40,11 +34,5 @@ func (c *config) Load() {
 func (c *config) Validate() {
 	if c.URL == "" {
 		panic(fmt.Sprintf("%s - %s", "url", configurationFailed))
-	}
-	if c.AppKey == "" {
-		panic(fmt.Sprintf("%s - %s", "app_key", configurationFailed))
-	}
-	if c.SecretKey == "" {
-		panic(fmt.Sprintf("%s - %s", "secret_key", configurationFailed))
 	}
 }
