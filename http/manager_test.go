0bdff021156c1baf1a2c2825c04433218f3496f9
Gabriel Felix <crowleyfelix@gmail.com>
2018-01-12 02:15:48 -0200

    Testa manager

--- http/manager_test.go
+++ http/manager_test.go
@@ -3,67 +3,163 @@ package http
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/stone-payments/logistic-sdk-go/errors"
 )
+
+type mockInterceptor struct {
+	err              errors.Error
+	onRequestCalled  bool
+	onResponseCalled bool
+}
+
+func (m *mockInterceptor) OnRequest(req Request) Request {
+	m.onRequestCalled = true
+	return req
+}
+func (m *mockInterceptor) OnResponse(resp Response) (Response, errors.Error) {
+	m.onResponseCalled = true
+	return resp, m.err
+}
 
 var _ = Describe("Manager", func() {
 
 	var (
 		url          = "url"
 		interceptors []Interceptor
-		manager      = NewManager("url", interceptors...).(*manager)
+		man          *manager
 	)
 
+	BeforeEach(func() {
+		man = NewManager("url", interceptors...).(*manager)
+	})
 	Describe("NewManager", func() {
 		It("should set interceptors", func() {
-			Expect(manager.interceptors).To(Equal(interceptors))
+			Expect(man.interceptors).To(Equal(interceptors))
 		})
 		It("should set base url", func() {
-			Expect(manager.baseURL).To(Equal(url))
+			Expect(man.baseURL).To(Equal(url))
 		})
 	})
 	Describe("BuildURL", func() {
 		It("should build url with params", func() {
-			Expect(manager.BuildURL("/%v/%v/%v", 1, 2, 3)).To(Equal(url + "/1/2/3"))
+			Expect(man.BuildURL("/%v/%v/%v", 1, 2, 3)).To(Equal(url + "/1/2/3"))
 		})
 	})
-	PDescribe("Request", func() {
-		Context("When request failed", func() {
-			It("should call OnRequest of external interceptors event", func() {
+	Describe("Request", func() {
 
+		var (
+			req *request
+		)
+
+		var (
+			resp         *response
+			err          error
+			methodCalled bool
+			method       = func(_ string, _ Request) (Response, error) {
+				methodCalled = true
+				resp = new(response)
+				return resp, err
+			}
+		)
+		var (
+			externalInterceptor *mockInterceptor
+			localInterceptor    *mockInterceptor
+			paramInterceptor    *mockInterceptor
+		)
+
+		//output
+		var (
+			actualResp Response
+			actualErr  errors.Error
+		)
+
+		BeforeEach(func() {
+			req = new(request)
+			methodCalled = false
+			err = nil
+
+			externalInterceptor = new(mockInterceptor)
+			localInterceptor = new(mockInterceptor)
+			paramInterceptor = new(mockInterceptor)
+
+			Interceptors = []Interceptor{externalInterceptor}
+			man.interceptors = []Interceptor{localInterceptor}
+		})
+		JustBeforeEach(func() {
+			actualResp, actualErr = man.Request(method, url, req, paramInterceptor)
+		})
+
+		Context("When request failled", func() {
+			BeforeEach(func() {
+				err = errors.Build(0)
 			})
-			It("should call OnRequest of local interceptors event", func() {
-
+			It("should call OnRequest of external interceptors", func() {
+				Expect(externalInterceptor.onRequestCalled).To(BeTrue())
+				Expect(externalInterceptor.onResponseCalled).To(BeFalse())
 			})
-			It("should call OnRequest of parameterized interceptors event", func() {
-
+			It("should call OnRequest of local interceptors", func() {
+				Expect(localInterceptor.onRequestCalled).To(BeTrue())
+				Expect(localInterceptor.onResponseCalled).To(BeFalse())
+			})
+			It("should call OnRequest of parameterized interceptors", func() {
+				Expect(paramInterceptor.onRequestCalled).To(BeTrue())
+				Expect(paramInterceptor.onResponseCalled).To(BeFalse())
 			})
 			It("should return an error", func() {
-
+				Expect(actualResp).To(BeNil())
+				Expect(actualErr).To(BeAssignableToTypeOf(errors.NewCallout()))
 			})
 		})
 		Context("When request success", func() {
-			Context("and interceptor return an error", func() {
-				It("should not call next interceptors", func() {
-
+			Context("and external interceptors return an error", func() {
+				BeforeEach(func() {
+					externalInterceptor.err = errors.Build(0)
 				})
 				It("should return an error", func() {
-
+					Expect(actualErr).To(BeAssignableToTypeOf(externalInterceptor.err))
+				})
+				It("should not call next interceptors", func() {
+					Expect(localInterceptor.onResponseCalled).To(BeFalse())
+					Expect(paramInterceptor.onResponseCalled).To(BeFalse())
+				})
+			})
+			Context("and local interceptors return an error", func() {
+				BeforeEach(func() {
+					localInterceptor.err = errors.Build(0)
+				})
+				It("should return an error", func() {
+					Expect(actualErr).To(BeAssignableToTypeOf(localInterceptor.err))
+				})
+				It("should not call next interceptors", func() {
+					Expect(paramInterceptor.onResponseCalled).To(BeFalse())
+				})
+			})
+			Context("and parameterized interceptors return an error", func() {
+				BeforeEach(func() {
+					paramInterceptor.err = errors.Build(0)
+				})
+				It("should return an error", func() {
+					Expect(actualErr).To(BeAssignableToTypeOf(paramInterceptor.err))
 				})
 			})
 			Context("and executed with success", func() {
-				It("should call OnResponse of external interceptors event", func() {
-
-				})
-				It("should call OnResponse of local interceptors event", func() {
-
-				})
-				It("should call OnResponse of parameterized interceptors event", func() {
-
-				})
 				It("should return response", func() {
-
+					Expect(actualErr).To(BeNil())
+					Expect(actualResp).To(Equal(resp))
 				})
 			})
+		})
+	})
+	Describe("allInterceptors", func() {
+		var all []Interceptor
+
+		BeforeEach(func() {
+			Interceptors = []Interceptor{new(mockInterceptor)}
+			man.interceptors = []Interceptor{new(mockInterceptor)}
+			all = man.allInterceptors(new(mockInterceptor))
+		})
+		It("should return all available interceptors", func() {
+			Expect(all).To(HaveLen(3))
 		})
 	})
 })
